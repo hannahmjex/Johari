4ee31200dfdf2adf0ecc01299aba590d73013b2a
Alex Field <38266956+alexfield55@users.noreply.github.com>
2021-09-24 16:54:00 -0600

    check boxes added

--- Johari/Pages/Clients/Index.cshtml.cs
+++ Johari/Pages/Clients/Index.cshtml.cs
@@ -6,14 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ApplicationCore.Models;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Johari.Pages.Clients
 {
     public class IndexModel : PageModel
     {
+        private readonly IUnitofWork _unitofWork;
+        public IndexModel(IUnitofWork unitofWork)
+        {
+            _unitofWork = unitofWork;
+        }
+
+        [BindProperty]
+        public IList<SelectListItem> Adjectives { get; set; }
+
+
         public void OnGet()
         {
-
+            List<Adjective> AdjectiveList = new List<Adjective>();
+            AdjectiveList = (List<Adjective>)_unitofWork.Adjective.List();
+            Adjectives = AdjectiveList.ToList<Adjective>()
+                .Select(c => new SelectListItem { Text = c.AdjName + " " +c.AdjDefinition, Value = c.AdjectiveID.ToString() })
+                .ToList<SelectListItem>();
         }
             
     }
