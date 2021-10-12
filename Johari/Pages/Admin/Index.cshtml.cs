3114c24f96a4a21c1d6a2efabe30f828775c6dfa
Alex Field <38266956+alexfield55@users.noreply.github.com>
2021-10-11 18:52:10 -0600

    friend/client database inserts working, html display working

--- Johari/Pages/Admin/Index.cshtml.cs
+++ Johari/Pages/Admin/Index.cshtml.cs
@@ -21,6 +21,12 @@ namespace Johari.Pages.Admin
         [BindProperty]
         public AdjectiveVM adjObj { get; set; }
 
+        public string hidden { get; set; }
+        public string unknown { get; set; }
+        public string blind { get; set; }
+        public string open { get; set; }
+
+
         public IEnumerable<ClientResponses> clientResponses { get; set; }
         public IEnumerable<FriendResponses> friendResponses { get; set; }
         public IEnumerable<Adjective> adjectives { get; set; }
@@ -56,6 +62,30 @@ namespace Johari.Pages.Admin
                     UnknownList = new List<string>()
                 };
 
+                //open query
+                var openQuery = from adjective in adjectives
+                                join fr in friendResponses on adjective.AdjectiveID equals fr.Adjective.AdjectiveID into frAdjTbl
+                                from frAdj in frAdjTbl.DefaultIfEmpty()
+
+                                join cr in clientResponses on adjective.AdjectiveID equals cr.Adjective.AdjectiveID into crAdjTbl
+                                from crAdj in crAdjTbl.DefaultIfEmpty()
+
+                                where frAdj != null && crAdj!=null
+
+                                   select new
+                                   {
+                                       AdjectiveName = adjective.AdjName,
+                                       AdjectiveType = adjective.AdjType,
+                                       AdjectiveDefinition = adjective.AdjDefinition
+                                   };
+
+                foreach (var o in openQuery)
+                {
+                    adjObj.OpenList.Add(o.AdjectiveName + " ");
+                }
+
+                open = listToString(adjObj.OpenList);
+
                 //hidden query
                 var unknownQuery = from adjective in adjectives
                                        join fr in friendResponses on adjective.AdjectiveID equals fr.Adjective.AdjectiveID into frAdjTbl
@@ -66,7 +96,7 @@ namespace Johari.Pages.Admin
 
                                        where frAdj == null && crAdj == null
 
-                                       select new
+                                   select new
                                        {
                                            AdjectiveName = adjective.AdjName,
                                            AdjectiveType = adjective.AdjType,
@@ -75,8 +105,10 @@ namespace Johari.Pages.Admin
 
                 foreach (var u in unknownQuery)
                 {
-                    adjObj.HiddenList.Add(u.AdjectiveName + " ");
+                    adjObj.UnknownList.Add(u.AdjectiveName + " ");
                 }
+
+                unknown = listToString(adjObj.UnknownList);
 
                 //hidden query
                 var hiddenQuery = from cr in clientResponses
@@ -98,6 +130,31 @@ namespace Johari.Pages.Admin
                     adjObj.HiddenList.Add(h.AdjectiveName + " ");
                 }
 
+                hidden = listToString(adjObj.HiddenList);
+
+                //blind query
+                var blindQuery = from fr in friendResponses
+                                  join cr in clientResponses on fr.Adjective.AdjectiveID equals cr.Adjective.AdjectiveID into crFrTbl
+                                  from crFr in crFrTbl.DefaultIfEmpty()
+
+                                  join adj in adjectives on fr.Adjective.AdjectiveID equals adj.AdjectiveID into crFrAdjTbl
+                                  from crFrAdj in crFrAdjTbl.DefaultIfEmpty()
+
+                                  where crFr == null
+                                  select new
+                                  {
+                                      AdjectiveName = crFrAdj.AdjName,
+                                      AdjectiveType = crFrAdj.AdjType,
+                                      AdjectiveDefinition = crFrAdj.AdjDefinition
+                                  };
+
+                foreach (var b in blindQuery)
+                {
+                    adjObj.BlindList.Add(b.AdjectiveName + " ");
+                }
+
+                blind = listToString(adjObj.BlindList);
+
             }
 
             //first time page load
@@ -115,6 +172,16 @@ namespace Johari.Pages.Admin
         public IActionResult OnPost()
         {
             return RedirectToPage("./Index", new { clientObj.Clients.ClientID });
+        }
+
+        public string listToString(List<string> sList)
+        { 
+            string ret = "";
+            foreach(string s in sList)
+            {
+                ret += s + " ";
+            }
+            return ret;
         }
     }
 }
